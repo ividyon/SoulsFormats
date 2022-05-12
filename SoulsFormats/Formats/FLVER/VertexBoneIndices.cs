cbf796036386e9af60021b576b8118789a7868b6
Nordgaren <nordgarentv@gmail.com>
2022-05-12 15:54:19 -0700

    Update VertexBoneIndices.cs
    Fixed diff problem

--- SoulsFormats/Formats/FLVER/VertexBoneIndices.cs
+++ SoulsFormats/Formats/FLVER/VertexBoneIndices.cs
@@ -9,11 +9,7 @@ namespace SoulsFormats
         /// <summary>
         /// Four indices of bones to bind a vertex to, accessed like an array. Unused bones should be set to 0.
         /// </summary>
-<<<<<<< HEAD
         public struct VertexBoneIndices : IEnumerable
-=======
-        public struct VertexBoneIndices: IEnumerable<int>
->>>>>>> b2ecbfe895c8f7619366a0ee196e6bfa9b08c39b
         {
             private int A, B, C, D;
 
@@ -53,29 +49,10 @@ namespace SoulsFormats
                     }
                 }
             }
-
-<<<<<<< HEAD
+            
             private List<int> VBIs => new List<int> {A, B, C, D};
             public IEnumerator<int> GetEnumerator() => VBIs.GetEnumerator();
             IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-=======
-            /// <summary>
-            /// Enumerates through all four bone indicies
-            /// </summary>
-            /// <returns></returns>
-            public IEnumerator<int> GetEnumerator()
-            {
-                yield return A;
-                yield return B;
-                yield return C;
-                yield return D;
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
->>>>>>> b2ecbfe895c8f7619366a0ee196e6bfa9b08c39b
         }
     }
 }
